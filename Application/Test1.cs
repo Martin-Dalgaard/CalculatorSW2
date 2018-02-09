9e4f8fcec21f496691d86f1095ea9e7707ca8c48
Dirk <me@home>
2018-02-09 18:27:01 +0100

    calculator

---
+++ Application/Test1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Calculator
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void Add2Plus2()
+        {
+            Calculator smart = new Calculator();
+            Assert.That(smart.Add(2,2).Equals(4));
+        }
+        [Test]
+        public void Multiply2By2()
+        {
+            Calculator smart = new Calculator();
+            Assert.That(smart.Multiply(2, 2).Equals(4));
+        }
+    }
+}
