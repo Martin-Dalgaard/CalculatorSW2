6dd0f6cb46ecb62ab94325a2d68d44f407b88e97
Dirk <me@home>
2018-02-09 19:32:18 +0100

    Test

---
+++ Calculator.Test/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void Add2Plus2()
+        {
+            Calculator smart = new Calculator();
+            Assert.That(smart.Add(2, 2).Equals(4));
+        }
+        [Test]
+        public void Multiply2By2()
+        {
+            Calculator smart = new Calculator();
+            Assert.That(smart.Multiply(2, 2).Equals(4));
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69cc59a8-7ca8-4c61-81ef-428ff0ec7acc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
